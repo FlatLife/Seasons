e16445f89201237065f57fee5140f81b3ebdf540
Josh Ryan <machmatic@gmail.com>
2017-01-22 21:01:55 +1300

    Added WalkTest prototype

---
+++ Prototype/WalkTest/Assets/Player.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public float speed;
+
+	// Update is called once per frame
+	void Update () {
+	  	float horizontalInput = Input.GetAxis("Horizontal");
+	  	float verticalInput = Input.GetAxis("Vertical");
+		// Move the player object
+		this.GetComponent<Rigidbody2D>().velocity = new Vector3(horizontalInput * speed,verticalInput * speed,0);
+		transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y);
+	}
+}
