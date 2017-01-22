0e14016f669130cd70e7e44d7c73dd6acc08ecbc
blueshroom <gavo.t@hotmail.com>
2017-01-22 21:19:05 +1300

    Added fire and food prefabs. Also added fire script

---
+++ Prototype/InventoryTest/Assets/Scripts/Fire.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+    public Transform itemPrefab;
+    public int cookCount;
+    float xCo;
+    float yCo;
+
+	// Use this for initialization
+	void Start () {
+        cookCount = 0;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void startCooking()
+    {
+        //Puts fish in position based on how many are already on the fire
+        if (cookCount == 1)
+        {
+            Transform food = Instantiate(itemPrefab);
+            food.position = new Vector3(getXCordinate() - 0.7f, getYCordinate() + 1, -1);
+        }
+        else if (cookCount == 2)
+        {
+            Transform food = Instantiate(itemPrefab);
+            food.position = new Vector3(getXCordinate() + 0.7f, getYCordinate() + 1, -1);
+        }
+        else if(cookCount == 0)
+        {
+            Transform food = Instantiate(itemPrefab);
+            food.position = new Vector3(getXCordinate(), getYCordinate() + 1, -1);
+        }
+        cookCount++;
+    }
+
+    //Returns the X cordinate of the object
+    public float getXCordinate()
+    {
+        return transform.position.x;
+    }
+
+    //Returns the Y cordinate of the object
+    public float getYCordinate()
+    {
+        return transform.position.y;
+    }
+}
