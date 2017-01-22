c7bcb38c95722aa8e477e3ee05cba2f0d1997747
blueshroom <gavo.t@hotmail.com>
2017-01-22 22:31:53 +1300

    Changed position of fish on fire

--- Prototype/InventoryTest/Assets/Scripts/Fire.cs
+++ Prototype/InventoryTest/Assets/Scripts/Fire.cs
@@ -7,15 +7,46 @@ public class Fire : MonoBehaviour {
     public int cookCount;
     float xCo;
     float yCo;
+    // An array with the sprites used for animation
+    public Sprite[] animSprites;
 
-	// Use this for initialization
-	void Start () {
+    // Controls how fast to change the sprites when
+    // animation is running
+    public float framesPerSecond;
+
+    // Reference to the renderer of the sprite
+    // game object
+    SpriteRenderer animRenderer;
+
+    // Time passed since the start of animatin
+    private float timeAtAnimStart;
+
+    // Indicates whether animation is running or not
+    private bool animRunning = false;
+
+    // Use this for initialization
+    void Start () {
         cookCount = 0;
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        // Get a reference to game object renderer and
+        // cast it to a Sprite Renderer
+        animRenderer = GetComponent<Renderer>() as SpriteRenderer;
+
+    }
+
+    void FixedUpdate()
+    {
+        if (!animRunning)
+        {
+                // Animation will start playing
+                animRunning = true;
+
+                // Record time at animation start
+                timeAtAnimStart = Time.timeSinceLevelLoad;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 
@@ -25,17 +56,17 @@ public class Fire : MonoBehaviour {
         if (cookCount == 1)
         {
             Transform food = Instantiate(itemPrefab);
-            food.position = new Vector3(getXCordinate() - 40, getYCordinate() + 50, 0);
+            food.position = new Vector3(getXCordinate() - 35, getYCordinate() + 55, 0);
         }
         else if (cookCount == 2)
         {
             Transform food = Instantiate(itemPrefab);
-            food.position = new Vector3(getXCordinate() + 40, getYCordinate() + 50, 0);
+            food.position = new Vector3(getXCordinate() + 45, getYCordinate() + 55, 0);
         }
         else if(cookCount == 0)
         {
             Transform food = Instantiate(itemPrefab);
-            food.position = new Vector3(getXCordinate(), getYCordinate() + 50, 0);
+            food.position = new Vector3(getXCordinate() + 5, getYCordinate() + 55, 0);
         }
         cookCount++;
     }
