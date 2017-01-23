5c72d6172a02ae78a61ce2b55a3772b02fa2eb91
Shaye McKay <smckay@oucs1534.otago.ac.nz>
2017-01-23 16:30:10 +1300

    Added the fishing spot

---
+++ Prototype/InventoryTest/Assets/Scripts/Fishing.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fishing : MonoBehaviour {
+
+	public float fishChance = 0.002f;
+	public Transform fishPrefab;
+	public bool isFishing = false;
+	public bool minigame = false;
+	public bool hasCaught = false;
+	public Transform alertP;
+	Transform alert;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isFishing && !minigame) {
+			if(Random.Range(0f,1f) < fishChance  && Time.timeScale != 0){ 
+				alert = Instantiate(alertP);
+				minigame = true;
+				Vector3 position = transform.position;
+				position.y += 2;
+				alert.position = position;
+				Destroy (alert.gameObject, 2.0f);
+			}
+		}
+
+		if (minigame && hasCaught) {
+			Transform fish = Instantiate(fishPrefab);
+			Vector3 position = transform.position;
+			position.x += 3;
+			fish.position =	position;
+			Destroy (alert.gameObject);
+			stop ();
+		}
+	}
+
+	public void fish(){
+		isFishing = true;
+	}	
+
+	public void stop(){
+		isFishing = false;
+		minigame = false;
+		hasCaught = false;
+	}
+}
