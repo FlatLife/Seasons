22c4398df47bc02506948d38c60c64a0756fb548
Max <max.newall@gmail.com>
2017-01-24 17:02:43 +1300

    Changed crafting to 2 slots, and the item crafted gets added to backpack. Also inadvertently removed the stacking of non-stackable items in the crafting table.

--- Prototype/Seasons/Assets/Scripts/CraftTable.cs
+++ Prototype/Seasons/Assets/Scripts/CraftTable.cs
@@ -7,11 +7,11 @@ public class CraftTable : MonoBehaviour {
 	public GameObject slotPrefab;
 	public float slotSize;
 
-	private Backpack BackPack;
+	public Backpack BackPack;
 	private RectTransform CraftTableRect;
 	public GameObject Slot1;
 	public GameObject Slot2;
-	public GameObject Slot3;
+
 
 
 
@@ -30,40 +30,38 @@ public class CraftTable : MonoBehaviour {
 
 			Slot1 = (GameObject)Instantiate(slotPrefab);
 			Slot2 = (GameObject)Instantiate(slotPrefab);
-			Slot3 = (GameObject)Instantiate(slotPrefab);
+
 
 			RectTransform slotRect1 = Slot1.GetComponent<RectTransform>();
 			RectTransform slotRect2 = Slot2.GetComponent<RectTransform>();
-			RectTransform slotRect3 = Slot3.GetComponent<RectTransform>();
+
 			CraftTableRect = GetComponent<RectTransform>();
 
 			Slot1.name = "Slot1";
 			Slot2.name = "Slot2";
-			Slot3.name = "Slot3";
+	
 
 			//Slot1.GetComponent<Button>().interactable = false;
 			Slot1.transform.SetParent(this.transform.parent);
 			Slot2.transform.SetParent(this.transform.parent);
-			Slot3.transform.SetParent(this.transform.parent);
+
 
 			//Debug.Log(CraftTableRect.localPosition);
 
 			//places the slots in the inventory in each column, then row
-			slotRect1.localPosition = CraftTableRect.localPosition + new Vector3(10, -20);
+			slotRect1.localPosition = CraftTableRect.localPosition + new Vector3(60, -20);
 			slotRect1.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, slotSize);
 			slotRect1.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, slotSize);
 
-			slotRect2.localPosition = CraftTableRect.localPosition + new Vector3(90, -20);			
+			slotRect2.localPosition = CraftTableRect.localPosition + new Vector3(130, -20);			
 			slotRect2.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, slotSize);
 			slotRect2.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, slotSize);
 
-			slotRect3.localPosition = CraftTableRect.localPosition + new Vector3(170, -20);		
-			slotRect3.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, slotSize);
-			slotRect3.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, slotSize);
+			
 			
 			Slot1.GetComponent<Image>().enabled = false;
 			Slot2.GetComponent<Image>().enabled = false;
-			Slot3.GetComponent<Image>().enabled = false;
+	
 		
 	}
 	
@@ -73,14 +71,16 @@ public class CraftTable : MonoBehaviour {
 	public void CheckRecipe(ItemType type1, ItemType type2, string product) {
         Slot tmp1 = Slot1.GetComponent<Slot>();
         Slot tmp2 = Slot2.GetComponent<Slot>();
-        Slot tmp3 = Slot3.GetComponent<Slot>();
 
+		if(!tmp1.isEmpty && !tmp2.isEmpty) {
 		if((tmp1.CurrentItem.type == type1 && tmp2.CurrentItem.type == type2)
         || (tmp1.CurrentItem.type == type2 && tmp2.CurrentItem.type == type1)){
             Item result = Resources.Load<GameObject>(product).GetComponent<Item>();
-            tmp3.AddItem(result);
+            BackPack.AddItem(result);
+			//BackPack.
             tmp1.DestroyItem ();
             tmp2.DestroyItem ();
         }
+		}
 	}
 }
