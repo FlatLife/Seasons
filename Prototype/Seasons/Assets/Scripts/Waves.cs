3246fb253afdc4268f836762af6cd60bc091a838
Shaye McKay <smckay@oucs1534.otago.ac.nz>
2017-01-24 15:49:00 +1300

    Added in water to fishing test.

---
+++ Prototype/Seasons/Assets/Scripts/Waves.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Waves : MonoBehaviour {
+
+	public Color c1;
+	public int lengthOfLineRenderer = 20;
+
+	void Start() {
+
+		LineRenderer lineRenderer = gameObject.GetComponent<LineRenderer>();
+		lineRenderer.material = new Material (Shader.Find("Particles/Additive (Soft)"));
+		lineRenderer.startWidth = 1.2f;
+		lineRenderer.endWidth = 1.2f;
+		lineRenderer.numPositions = lengthOfLineRenderer;
+		lineRenderer.numCornerVertices = 10;
+		lineRenderer.numCapVertices = 10;
+		lineRenderer.sortingLayerName = "Water";
+
+	}
+
+	void Update() {
+		
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+		BuoyancyEffector2D effector = GetComponent<BuoyancyEffector2D> ();
+		LineRenderer lineRenderer = GetComponent<LineRenderer>();
+		lineRenderer.startColor = c1;
+		lineRenderer.endColor = c1;
+		EdgeCollider2D collider = GetComponent<EdgeCollider2D> ();
+		var points = new Vector3[lengthOfLineRenderer];
+		var points2D = new Vector2[lengthOfLineRenderer];
+		var t = Time.time;
+		for(int i = 0; i < lengthOfLineRenderer; i++) {
+			points[i] = new Vector3(i * 0.5f, 0.3f * Mathf.Sin(i + 2*t), 0.0f);
+			points2D[i] = new Vector2(i * 0.5f, 0.3f * Mathf.Sin(i + 2*t));
+		}
+		effector.surfaceLevel = points[(int)Math.Ceiling(player.transform.position.x+7.3f)].y;
+		lineRenderer.SetPositions(points);
+		collider.points = points2D;
+	}
+}
