2ad135eac1d696bee1f0a050bccab6c4b8130dea
Gavin Trebilcock <gavo.t@hotmail.com>
2017-01-25 17:03:47 +1300

    Added changes to Cooking Files but not working yet, heading home.

--- Prototype/Seasons/Assets/Scripts/Food.cs
+++ Prototype/Seasons/Assets/Scripts/Food.cs
@@ -6,11 +6,9 @@ public class Food : MonoBehaviour {
 
     public int itemPosition;
     float timeCooked;
-    public Sprite raw;
-    public Sprite cooked;
-    public Sprite burnt;
+    public bool cooking;
 
-    private SpriteRenderer spriteRenderer;
+    public SpriteRenderer spriteRenderer;
 
     public CookingUI cookingUI;
 
@@ -18,39 +16,10 @@ public class Food : MonoBehaviour {
     void Start () {      
         cookingUI = GameObject.Find("CookingUI").GetComponent<CookingUI>();
         spriteRenderer = GetComponent<SpriteRenderer>();
-        // if the sprite on spriteRenderer is null then set the sprite to raw
-        if (spriteRenderer.sprite == null)
-        {
-            spriteRenderer.sprite = raw;
-        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
         
-        timeCooked = timeCooked + Time.deltaTime;
-        if (timeCooked >= 5 && spriteRenderer.sprite == raw) 
-        {
-            spriteRenderer.sprite = cooked;
-            if(itemPosition == 1){
-                cookingUI.ChangeItem(itemPosition, "CookedFish");
-            } else if(itemPosition == 2){
-                cookingUI.ChangeItem(itemPosition, "CookedFish");
-            } else if(itemPosition == 3){
-                cookingUI.ChangeItem(itemPosition, "CookedFish");
-            }
-        }
-        
-        if(timeCooked >= 10 && spriteRenderer.sprite == cooked)
-        {
-            spriteRenderer.sprite = burnt;
-            if(itemPosition == 1){
-                cookingUI.ChangeItem(itemPosition, "BurntFish");
-            } else if(itemPosition == 2){
-                cookingUI.ChangeItem(itemPosition, "BurntFish");
-            } else if(itemPosition == 3){
-                cookingUI.ChangeItem(itemPosition, "BurntFish");
-            }
-        }
 	}
 }
