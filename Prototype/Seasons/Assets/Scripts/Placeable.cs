14b302776142908d2457d3cf3a5f21348b1a0253
Shaye McKay <mcksh759@student.otago.ac.nz>
2017-02-01 16:34:26 +1300

    Half working hover item placement.

---
+++ Prototype/Seasons/Assets/Scripts/Placeable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Placeable : MonoBehaviour {
+
+	public bool colliding;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.tag != "Build"){
+			colliding = true;
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D other){
+		if(other.tag != "Build"){
+			colliding = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if(other.tag != "Build"){
+			colliding = false;
+		}
+	}
+}
