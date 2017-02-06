c3413e96df2db066cdce11191ebc1ba9f2250af4
Shaye Mckay <mcksh759@student.otago.ac.nz>
2017-02-06 22:10:26 +1300

    Added a few use item statements and fixed placing collisions

--- Prototype/Seasons/Assets/Scripts/Item.cs
+++ Prototype/Seasons/Assets/Scripts/Item.cs
@@ -17,6 +17,9 @@ public class Item : MonoBehaviour {
 	public bool isCrop;
 	public bool isFinishedCrop;
 
+	public Slot slot;
+	private Item newItem;
+	private BoxCollider2D collider;
 	private GameObject hunger;
 	private GameObject thirst;
 	private PlaceObjects builder;
@@ -28,16 +31,16 @@ public class Item : MonoBehaviour {
 		bool toBeDeleted = true;
 		switch(type) {
 			case ItemType.FISHINGROD:
-				Debug.Log("FishingRodded");
+				toBeDeleted = false;
 				break;
 			case ItemType.STICK:
-				Debug.Log("Sticked");
+			toBeDeleted = false;
 				break;
 			case ItemType.VINE:
-				Debug.Log("Vined");
+				toBeDeleted = false;
 				break;
 			case ItemType.RAWFISH:
-				Debug.Log("Fished");
+				toBeDeleted = false;
 				break;
 			case ItemType.COOKEDFISH:
 				hunger  = GameObject.Find("HungerBar");
@@ -54,12 +57,50 @@ public class Item : MonoBehaviour {
 				toBeDeleted = durability == 0;
 				break;
 			case ItemType.FRESHWATER:
+				toBeDeleted = false;
 				thirst  = GameObject.Find("ThirstBar");
 				thirst.GetComponent<BarScript>().increment(0.3f);
+				slot.DestroyItem ();
+				newItem = Instantiate(Resources.Load<GameObject>("Bucket").GetComponent<Item>(), slot.transform);
+				collider = newItem.gameObject.GetComponent<BoxCollider2D>();
+				if(collider != null){
+					collider.enabled = false;
+				}
+				newItem.transform.position = new Vector3 (0,20f,0);
+				newItem.GetComponent<SpriteRenderer>().enabled = false;
+				slot.AddItem(newItem);
+				break;
+			case ItemType.SALTWATER:
+				toBeDeleted = false;
+				slot.DestroyItem ();
+				newItem = Instantiate(Resources.Load<GameObject>("Bucket").GetComponent<Item>(), slot.transform);
+				collider = newItem.gameObject.GetComponent<BoxCollider2D>();
+				if(collider != null){
+					collider.enabled = false;
+				}
+				newItem.transform.position = new Vector3 (0,20f,0);
+				newItem.GetComponent<SpriteRenderer>().enabled = false;
+				slot.AddItem(newItem);
+
 				break;
 			case ItemType.WATERPURIFIER:
 				builder = GameObject.Find("Main Camera").GetComponent<PlaceObjects>();
 				builder.build("WaterPurifier", "PlaceWater");
+				break;
+			case ItemType.BUCKET:
+				toBeDeleted = false;
+				bool nearWater = GameObject.Find ("Player").GetComponent<Player> ().switchSwimMode;
+				if (nearWater) {
+				slot.DestroyItem ();
+				newItem = Instantiate(Resources.Load<GameObject>("SaltWater").GetComponent<Item>(), slot.transform);
+				collider = newItem.gameObject.GetComponent<BoxCollider2D>();
+				if(collider != null){
+					collider.enabled = false;
+				}
+				newItem.transform.position = new Vector3 (0,20f,0);
+				newItem.GetComponent<SpriteRenderer>().enabled = false;
+				slot.AddItem(newItem);
+				}
 				break;
 		}
 		return toBeDeleted;
