62c50ff24f77f4cc864699d80f94127f1b60ffc2
Shaye McKay <mcksh759@student.otago.ac.nz>
2017-02-06 16:28:20 +1300

    E key popup appears when you can use something. also fixed bug with creating items collisions

---
+++ Prototype/Seasons/Assets/Scripts/GUIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIManager : MonoBehaviour {
+	public Player player;
+
+	private GameObject popup;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(player.atUse){
+			if(popup == null){
+				popup = Instantiate(Resources.Load<GameObject>("EKey"), this.transform);
+			}
+			Vector3 pos = player.transform.position;
+			pos.y += 2f; 
+			popup.transform.position = pos;
+		}else{
+			Destroy(popup);
+		}
+	}
+}
