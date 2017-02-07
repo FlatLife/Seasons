ff465773d44c27fcc33090fa4970e156d0ef8313
Shaye McKay <mcksh759@student.otago.ac.nz>
2017-02-07 15:40:55 +1300

    Changed size of water purifier ui

--- Prototype/Seasons/Assets/Scripts/WaterPurifierUI.cs
+++ Prototype/Seasons/Assets/Scripts/WaterPurifierUI.cs
@@ -14,15 +14,16 @@ public class WaterPurifierUI : MonoBehaviour {
 
 	public void Initialize() {
 		RectTransform uiRect = this.GetComponent<RectTransform>();
-		uiRect.sizeDelta = new Vector3(slotSize * 2 + slotSize, slotSize + slotSize/2, 0);
-		uiRect.position = new Vector3(400,200);
+		uiRect.sizeDelta = new Vector3(228, 123, 0);
+		uiRect.position = new Vector3(400,400);
 
 		FreshWaterSlot = Instantiate(slotPrefab);
 		FreshWaterSlot.name = "FreshWaterSlot";
 		RectTransform FreshwaterRect = FreshWaterSlot.GetComponent<RectTransform>();
 		FreshWaterSlot.transform.SetParent(this.transform);
-		FreshwaterRect.localPosition = new Vector3(slotSize/(2+1) + (slotSize/(2+1) + slotSize) * 1, -slotSize/4);
-		FreshwaterRect.sizeDelta = new Vector3(slotSize, slotSize);
+		FreshwaterRect.localPosition = new Vector3(60, -40);
+		FreshwaterRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, slotSize);
+		FreshwaterRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, slotSize);
 
 		FreshWaterSlot.GetComponent<Image>().enabled = false;
 
@@ -30,8 +31,9 @@ public class WaterPurifierUI : MonoBehaviour {
 		SaltWaterSlot.name = "SaltWaterSlot";
 		RectTransform SaltWaterRect = SaltWaterSlot.GetComponent<RectTransform>();
 		SaltWaterSlot.transform.SetParent(this.transform);
-		SaltWaterRect.localPosition = new Vector3(slotSize/(2+1) + (slotSize/(2+1) + slotSize) * 0, -slotSize/4);
-		SaltWaterRect.sizeDelta = new Vector3(slotSize, slotSize);
+		SaltWaterRect.localPosition = new Vector3(130, -40);
+		SaltWaterRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, slotSize);
+		SaltWaterRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, slotSize);
 		SaltWaterSlot.GetComponent<Image>().enabled = false;
 	}
 }
