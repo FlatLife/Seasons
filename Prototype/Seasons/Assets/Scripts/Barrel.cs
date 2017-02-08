a70ac7c732c14d92b674dd84409a6540a7cbea1b
Max <max.newall@gmail.com>
2017-02-08 17:21:40 +1300

    Barrel works, just need to add bar to it and arrows

--- Prototype/Seasons/Assets/Scripts/Barrel.cs
+++ Prototype/Seasons/Assets/Scripts/Barrel.cs
@@ -6,8 +6,9 @@ public class Barrel : MonoBehaviour {
 
 	public Canvas canvas;
 	public BarrelUI BarrelUI;
-	//public float waterPurify = 3f;
-	// Use this for initialization
+	// set our barrel fill bar to 0, contains nothing 
+	public int barrelFill = 0;
+	public Backpack backpack;
 
 	void Awake () {
 		canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
@@ -19,6 +20,28 @@ public class Barrel : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//empty slot
+	if(!BarrelUI.bucketEmptySlot.isEmpty){
+		if(BarrelUI.bucketEmptySlot.CurrentItem.type == ItemType.FRESHWATER) {
+			barrelFill++;
+			BarrelUI.bucketEmptySlot.ClearSlot();
+			Item item = Instantiate(Resources.Load<Item> ("Bucket"));
+			item.transform.position = new Vector3 (0,20f,0);
+			BarrelUI.bucketEmptySlot.AddItem(item);
+		}
+	}
+		//fill slot
+	if(!BarrelUI.bucketFillSlot.isEmpty) {
+		if(BarrelUI.bucketFillSlot.CurrentItem.type == ItemType.BUCKET) {
+				if(barrelFill > 0) {
+					barrelFill--;
+					BarrelUI.bucketFillSlot.ClearSlot();
+					Item item = Instantiate(Resources.Load<Item> ("FreshWater"));
+					item.transform.position = new Vector3 (0,20f,0);
+					BarrelUI.bucketFillSlot.AddItem(item);
+				}
+			}
+		}
 	}
 }
+
