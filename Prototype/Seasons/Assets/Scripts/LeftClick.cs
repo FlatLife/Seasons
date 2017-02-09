0f9059b24a147db0402e2268aae21213c45a088c
Shaye Mckay <mcksh759@student.otago.ac.nz>
2017-02-09 20:45:04 +1300

    fixed bug for move items hover

---
+++ Prototype/Seasons/Assets/Scripts/LeftClick.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;// Required when using Event data.
+
+public class LeftClick : MonoBehaviour, IPointerDownHandler// required interface when using the OnPointerDown method.
+{
+
+	public GameObject canvas;
+
+	void Awake(){
+		canvas = GameObject.Find ("Canvas");
+	}	
+	//Do this when the mouse is clicked over the selectable object this script is attached to.
+	public void OnPointerDown (PointerEventData eventData) 
+	{
+		if(eventData.button == PointerEventData.InputButton.Left){
+			Debug.Log (gameObject);
+			canvas.GetComponentInChildren<Backpack> ().MoveItem(gameObject);
+		}
+	}
+}
