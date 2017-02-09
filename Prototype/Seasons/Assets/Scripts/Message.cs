8f055c886f6891086ec2183d26ba7c794b95ef3b
Shaye McKay <mcksh759@student.otago.ac.nz>
2017-02-09 16:18:25 +1300

    fixed messages

--- Prototype/Seasons/Assets/Scripts/Message.cs
+++ Prototype/Seasons/Assets/Scripts/Message.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class Message : MonoBehaviour {
 
+	public float cooldown;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
+
+	void Update(){
+		Time.timeScale = 0f;
+		cooldown -= Time.unscaledDeltaTime;
+		if(Input.anyKeyDown && cooldown < 0){
+			close();
+		}
+	}
 	
 	// Update is called once per frame
 	public void close(){
+		Time.timeScale = 1f;
 		Destroy (this.gameObject);
 	}
 }
