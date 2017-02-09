f5edb57dcad4b6773a6836f54409c471e082db47
Shaye Mckay <mcksh759@student.otago.ac.nz>
2017-02-09 21:17:53 +1300

    Volume bar working in pause menu

---
+++ Prototype/Seasons/Assets/Scripts/VolumeManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeManager : MonoBehaviour {
+
+	// Use this for initialization
+	public Slider volumeSlider;
+	public AudioSource volumeAudio;
+
+	public void VolumeController(){
+		volumeAudio.volume = volumeSlider.value;
+	}
+}
