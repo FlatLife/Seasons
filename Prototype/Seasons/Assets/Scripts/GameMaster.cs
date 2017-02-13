cd854ebbf3459ee422fa33e6a478d2ef8d945632
Gavin Trebilcock <gavo.t@hotmail.com>
2017-02-13 13:02:06 +1300

    Added Winter music to game and added fading in and out code too music

--- Prototype/Seasons/Assets/Scripts/GameMaster.cs
+++ Prototype/Seasons/Assets/Scripts/GameMaster.cs
@@ -6,5 +6,66 @@ public class GameMaster : MonoBehaviour {
 
 	public static int dayCount;
 	public static bool isWinter;
+
+	public AudioSource music;
+	public AudioClip summerMusic;
+	public AudioClip winterMusic;
+	private static float fadeVolume = 2;
+	private static bool fadingMusic = false;
+	private static bool fadingOut;
+	private static bool enteringWinter;
+
+	void Start(){
+		music = GetComponent<AudioSource>();
+		music.clip = summerMusic;
+		music.Play();
+		music.loop = true;
+	}
+	void Update(){
+		if(isWinter && music.clip == summerMusic){
+			music.clip = winterMusic;
+			music.Play();
+			music.loop = true;
+		}
+
+		if(!isWinter && music.clip == winterMusic){
+			music.clip = summerMusic;
+			music.Play();
+			music.loop = true;
+		}
+
+		if(fadingMusic){
+			if(fadingOut){
+				fadeVolume -= Time.deltaTime;
+				music.volume = fadeVolume/2;
+				if(music.volume == 0){
+					fadingOut = false;
+					if(enteringWinter){
+						music.clip = winterMusic;
+					} else {
+						music.clip = summerMusic;
+					}
+					music.volume = 0;
+					music.Play();
+					music.loop = true;
+				}
+			} else {
+				fadeVolume += Time.deltaTime;
+				music.volume = fadeVolume/2;
+				if(music.volume == 2){
+					fadingMusic = false;
+				}
+			}
+		}
+		
+	}
+
+	public static void fadeMusic(bool winter){
+		fadingMusic = true;
+		fadingOut = true;
+		if(winter){
+			enteringWinter = true;
+		}
+	}
 	
 }
