0c60378af54cdde1ecd30dfe645e94985c4a53a0
Shaye McKay <mcksh759@student.otago.ac.nz>
2017-02-17 18:43:09 +1300

    reverted back to old trigger system for placing objects

--- Prototype/Seasons/Assets/Scripts/Placeable.cs
+++ Prototype/Seasons/Assets/Scripts/Placeable.cs
@@ -5,28 +5,27 @@ using UnityEngine;
 public class Placeable : MonoBehaviour {
 
 	public bool colliding;
-	private List<Collider2D> objectColliderID;
+	private Collider2D objectColliderID;
 	// Use this for initialization
 	void Start () {
-		objectColliderID = new List<Collider2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		colliding = objectColliderID.Count != 0;
 	}
 
-	void OnTriggerEnter2D(Collider2D other){
-			objectColliderID.Add(other);
+	void OnTriggerStay2D(Collider2D other){
+			colliding = true;
+			objectColliderID = other;
 	}
 		
 
 	void OnTriggerExit2D(Collider2D other){
-		for(int i = objectColliderID.Count - 1; i >= 0; i--){
-			if(other == objectColliderID[i]){
-				objectColliderID.RemoveAt(i);
+			if(other == objectColliderID){
+				colliding = false;
+				objectColliderID = null;
 			}
 		}
 		
 	}
-}
+
